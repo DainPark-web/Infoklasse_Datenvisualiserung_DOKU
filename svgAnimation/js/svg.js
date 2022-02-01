cb946db0e4903190d0fe0fcc077f6c2c17a86c24
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-01 20:07:37 +0100

    update

--- svgAnimation/js/svg.js
+++ svgAnimation/js/svg.js
@@ -7,6 +7,7 @@ const mouth = document.querySelector(".cls-2");
 
 const mouthR = document.querySelector(".eyeR");
 const mouthL = document.querySelector(".eyeL");
+const tuk = document.querySelector(".tuk");
 
 console.log(svgSize.clientWidth);
 let mouseX = 0;
@@ -16,25 +17,39 @@ let fontPosX = -window.innerWidth/3;
 let fontPosY = -window.innerWidth/9;
 
 let eyeR = 20;
+
+let cliked = false;
+
 // mouthSvg
 document.addEventListener("mousedown", () => {
+
+    cliked = true;
     mouth.classList.toggle("newMouth")
     mouth.classList.toggle("mouthSvg");
+    tuk.classList.toggle("tuk");
+    tuk.classList.toggle("tukCliked");
     
-    mouthR.style.fill ="#ff0000"
-    mouthL.style.fill ="#fff"
+    mouthR.style.fill ="#00ffff"
+    mouthL.style.fill ="#000fff"
     mouthL.style.r = `${eyeR}`;
     mouthR.style.r = `${eyeR}`;
+    mouthL.style.cx = `${419} `;
+    mouthR.style.cx = `${50 }`;
 })
 
 document.addEventListener("mouseup", () => {
-   
+    cliked = false;
     mouth.classList.toggle("newMouth")
     mouth.classList.toggle("mouthSvg");
+    tuk.classList.toggle("tuk");
+    tuk.classList.toggle("tukCliked");
     mouthR.style.fill ="#000"
     mouthL.style.fill ="#000"
     mouthL.style.r = `50`;
     mouthR.style.r = `50`;
+    mouthL.style.cx = `50`;
+    mouthR.style.cx = `419`;
+    tuk.style.stroke = "#000";
 })
 
 document.addEventListener("mousemove", (e) => {
@@ -60,7 +75,11 @@ let moveUpDown = 0;
 let time = 0;
 function init(){
     time += 0.1;
-    
+    if(cliked){
+
+        mouthR.style.r = `${eyeR + Math.sin(time * 2) * 200}`;
+        mouthL.style.r = `${eyeR + Math.cos(time * 2) * 200}`;
+    }
     moveUpDown += Math.sin(time/2) * 2;
     faceM.setAttribute("transform", `translate(${mouseX}, ${mouseY + moveUpDown}) scale(1)`);
     window.requestAnimationFrame(init)
